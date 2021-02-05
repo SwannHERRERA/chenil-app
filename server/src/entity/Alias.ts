4b064356ae488c0455c1aec48523fb2ce2e8044a
Swann <swann@devloup.dev>
2021-02-05 10:26:21 +0100

    add entites

---
+++ server/src/entity/Alias.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
+@Entity()
+export class Alias {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => User, (user) => user.alias)
+  user: User;
+}
