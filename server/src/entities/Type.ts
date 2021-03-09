c52b8140d53436c71eb050645d1e2f4a3d734968
Swann <swann@devloup.dev>
2021-03-10 00:29:34 +0100

    Work in progress on type and type effect

--- server/src/entities/Type.ts
+++ server/src/entities/Type.ts
@@ -1,6 +1,10 @@
-import {Entity} from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Type {
+  @PrimaryGeneratedColumn("uuid")
+  typeId: string;
 
+  @Column()
+  name: string;
 }
--- server/src/entities/TypeEffect.ts
+++ server/src/entities/TypeEffect.ts
@@ -1,6 +1,17 @@
-import {Entity} from "typeorm";
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Type } from "./Type";
 
 @Entity()
 export class TypeEffect {
+  @PrimaryGeneratedColumn("uuid")
+  TypeEffectId: string;
 
+  @OneToOne(() => Type)
+  fromType: Type;
+
+  @OneToOne(() => Type)
+  toType: Type;
+
+  @Column()
+  coefficient: number;
 }
