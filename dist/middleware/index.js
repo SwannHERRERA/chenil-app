d4c23f1d4ed0b4f2fbe70c5f38682a82b37c2463
Swann <swann@devloup.dev>
2021-03-23 00:25:25 +0100

    remove dist

--- dist/middleware/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var error_handler_1 = require("./error-handler");
-Object.defineProperty(exports, "errorHandler", { enumerable: true, get: function () { return error_handler_1.errorHandler; } });
-var log_request_1 = require("./log-request");
-Object.defineProperty(exports, "logRequest", { enumerable: true, get: function () { return log_request_1.logRequest; } });
-var response_time_1 = require("./response-time");
-Object.defineProperty(exports, "responseTime", { enumerable: true, get: function () { return response_time_1.responseTime; } });
-//# sourceMappingURL=index.js.map
