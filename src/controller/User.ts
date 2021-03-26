52754c6895b2e55eafa778ef2990c3179f28a08e
HERRERA Swann <swann@devloup.Dev>
2021-03-26 23:07:08 +0100

    add refresh token route

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -1,4 +1,12 @@
+import { verify } from "jsonwebtoken";
 import { Context } from "koa";
+import { createAccessToken, createRefreshToken } from "../utils/token";
+import { User } from "../entities/User";
+
+interface JsonWebTokenResult {
+  userId: string;
+  tokenVersion: number;
+}
 
 export function home(ctx: Context) {
   ctx.body = "hello API World";
@@ -7,6 +15,38 @@ export function home(ctx: Context) {
 export function sendRefreshToken(ctx: Context, token: string): void {
   ctx.cookies.set("jid", token, {
     httpOnly: true,
-    path: "/refresh_token",
+    path: "/user/refresh_token",
   });
+}
+
+export async function refresh_tokens(ctx: Context): Promise<void> {
+  const token = ctx.cookies.get("jid");
+  if (!token) {
+    ctx.body = {status: 401, accessToken: "", ok: false};
+    return;
+  }
+
+  let user: User | undefined | null = null;
+  
+  try {
+    const payload: JsonWebTokenResult = verify(token!, process.env.REFRESH_TOKEN_SECRET!) as JsonWebTokenResult;
+    user = await User.findOne({ userId: payload.userId });
+    if (!user || user.tokenVersion !== payload.tokenVersion) {
+      throw new Error("no user or token invalid");
+    }
+  } catch (err) {
+    console.error(err);
+    ctx.body = {
+      ok: false,
+      accessToken: ""
+    };
+    return;
+  }
+
+  sendRefreshToken(ctx, createRefreshToken(user!));
+
+  ctx.body = {
+    ok: true,
+    accessToken: createAccessToken(user!)
+  };
 }
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,10 @@
 import Koa from "koa";
 import Router from "koa-router";
-import { home } from "../controller/User";
+import { home, refresh_tokens } from "../controller/User";
 
 export function init(server: Koa) {
   const router = new Router({ prefix: "/user" });
   router.get("/", home);
+  router.post("/refresh_token", refresh_tokens);
   server.use(router.routes());
 }
